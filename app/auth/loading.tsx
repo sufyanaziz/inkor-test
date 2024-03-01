20a4ce47aacdedc933bf2479ccc791d25c879fde
Achmad Sufyan Aziz <achmadsufyan98@gmail.com>
2024-03-01 20:35:03 +0700

    fix: add global card css and loading for auth

---
+++ app/auth/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="h-full w-full bg-cyan-50 flex justify-center items-center">
+      Loading
+    </div>
+  );
+};
+
+export default Loading;
--- common/components/Card.tsx
+++ common/components/Card.tsx
@@ -4,11 +4,9 @@ import React from "react";
 
 const Card = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="border border-solid">
-      <div className="p-5">
-        <h1 className="font-bold mb-5">React JS Test PT CYBER EDU INKOR</h1>
-        {children}
-      </div>
+    <div className="card border border-solid">
+      <h1 className="font-bold mb-5">React JS Test PT CYBER EDU INKOR</h1>
+      {children}
     </div>
   );
 };
